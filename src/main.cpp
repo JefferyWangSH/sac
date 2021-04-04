d763733c1419a01af7880d54c5243d833df52232
email <17307110117@fudan.edu.cn>
2021-04-04 11:47:53 +0800

    commit via git

--- src/main.cpp
+++ src/main.cpp
@@ -17,28 +17,36 @@ int main(int argc, char *argv[]) {
     std::string output_filename = "../results/output.txt";
 
     /* read params from command line */
-    boost::program_options::options_description opts("all options");
+    boost::program_options::options_description opts("Program options");
     boost::program_options::variables_map vm;
 
     opts.add_options()
-            ("h", "display this information")
-            ("?", "display this information")
-            ("lt", boost::program_options::value<int>(&lt), "imaginary-time lattice size of QMC simulation, default: 80")
-            ("beta", boost::program_options::value<double>(&beta), "inverse temperature of quantum system, default: 4.0")
-            ("nOmega", boost::program_options::value<int>(&nOmega), "number of slices in frequency space, default: 50")
+            ("help,h", "display this information")
+            ("lt,t", boost::program_options::value<int>(&lt), "imaginary-time lattice size of QMC simulation, default: 80")
+            ("beta,b", boost::program_options::value<double>(&beta), "inverse temperature of quantum system, default: 4.0")
+            ("nOmega,n", boost::program_options::value<int>(&nOmega), "number of slices in frequency space, default: 50")
             ("omegaMin", boost::program_options::value<double>(&omegaMin), "lower bound of frequency space, default: 0.0")
             ("omegaMax", boost::program_options::value<double>(&omegaMax), "upper bound of frequency space, default: 5.0")
             ("infile,i", boost::program_options::value<std::string>(&input_filename), "input filename, default: ../results/benchmark_g.txt")
             ("outfile,o", boost::program_options::value<std::string>(&output_filename), "output filename, default: ../results/output.txt");
+    try {
+        boost::program_options::store(parse_command_line(argc, argv, opts), vm);
+    }
+    catch (...) {
+        std::cerr << "Undefined options got from command line."<< std::endl;
+        exit(1);
+    }
 
-    boost::program_options::store(parse_command_line(argc, argv, opts), vm);
     boost::program_options::notify(vm);
 
-    if (vm.count("h") || vm.count("?")){
+    if (vm.count("help")) {
+        std::cerr << argv[0] << std::endl;
         std::cerr << opts << std::endl;
         exit(1);
     }
 
+
+    /* start SAC process */
     StochasticAC sac;
 
     sac.set_SAC_params(80, 4.0, 50, 0.0, 5.0);
