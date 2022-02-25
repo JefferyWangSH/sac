710701679310c8e3b225fde4441541dfdfc1ae76
Jeffery Wang <78529482+JefferyWangSH@users.noreply.github.com>
2022-02-26 02:19:19 +0800

    chore: modify building process with cmake

--- build/runcmake.sh
+++ build/runcmake.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" ..
-cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
+# load environment variables
+module purge
+module load cmake/3.21.2 
+module load gcc/10.2.0
+module load oneAPI/2022.1
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -G "CodeBlocks - Unix Makefiles" ..
+# cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -G "CodeBlocks - Unix Makefiles" ..
+
+# clear up environment variables
+module purge
 
 exit 0
--- run/run.sh
+++ run/run.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-#SBATCH --partition=v6_384
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-
-exe="../build/sac"
-input_name="benchmark"
-lt=150
-beta=6.0
-nbin_qmc=1000
-theta=1e8
-
-mpirun ${exe} --lt=${lt} --beta=${beta} --nbin-qmc=${nbin_qmc} --theta=${theta} --input-folder-name=${input_name}
+# execute program
+$exe --lt=$lt --beta=$beta --nbin-qmc=$nbin_qmc --theta=$theta --input-folder-name=$input_name
 
 exit 0
