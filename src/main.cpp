9b0a01149f01956942126e058473d676c4958666
Jeffery Wang <78529482+JefferyWangSH@users.noreply.github.com>
2022-02-27 14:51:59 +0800

    test: add github action

--- src/main.cpp
+++ src/main.cpp
@@ -172,7 +172,7 @@ int main(int argc, char *argv[]) {
     if (vm.count("help")) {
         std::cerr << argv[0] << std::endl;
         std::cerr << opts << std::endl;
-        exit(1);
+        return 0;
     }
 
     // time record
