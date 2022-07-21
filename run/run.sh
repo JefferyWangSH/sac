1cf0d2d23401ad66085e522b789e3efa2707b306
jefferywang <17307110117@fudan.edu.cn>
2022-07-21 15:09:29 +0800

    test: update shell scripts

--- run/run.sh
+++ run/run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# execute program
-$exe --kernel-type=$kernel_type --update-type=$update_type \
-     --tau-file-path=$tau_file_path --corr-file-path=$corr_file_path \
-     --log-file-path=$log_file_path --spec-file-path=$spec_file_path  --report-file-path=$report_file_path \
-     --lt=$lt --beta=$beta --nbin-qmc=$nbin_qmc --rebin-pace=$rebin_pace --nbootstrap=$nbootstrap \
-     --freq-min=$freq_min --freq-max=$freq_max --freq-interval=$freq_interval --spec-interval=$spec_interval --ndelta=$ndelta \
-     --theta=$theta --annealing-pace=$annealing_pace --stablization-pace=$stablization_pace \
-     --sbin-sac=$bin_size --nbin-sac=$bin_num --collecting-steps=$collecting_steps
+# execute the program
+$exe --config=$config_file --tgrids=$tgrids_file --corr=$corr_file \
+     --log=$log_file --spec=$spec_file  --report=$report_file
 
 exit 0
